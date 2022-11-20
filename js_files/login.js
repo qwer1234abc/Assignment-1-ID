fec6190af34bb5efeb6dea4e7c5d12d0bd1a2717
qwer1234abc <kelvinbehcy@gmail.com>
2022-11-20 11:16:49 +0800

    Added new login js file

---
+++ js_files/login.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+  userinput = document.querySelector(".userinput");
+  card = document.querySelector(".formcontainer");
+  card.classList.toggle("active");
+  userinput.classList.toggle("active");
+}
