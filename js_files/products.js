eea2b60b7da7c471bf6bf5a8bc8211e6cc717775
qwer1234abc <kelvinbehcy@gmail.com>
2022-12-01 00:51:02 +0800

    fomatting document

--- js_files/products.js
+++ js_files/products.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
-    $(".addtocart").on("click", function () {
-      var button = $(this);
-      var cart = $("#cart");
-      var cartTotal = cart.attr("data-totalitems");
-      var newCartTotal = parseInt(cartTotal) + 1;
-  
-      button.addClass("sendtocart");
+  $(".addtocart").on("click", function () {
+    var button = $(this);
+    var cart = $("#cart");
+    var cartTotal = cart.attr("data-totalitems");
+    var newCartTotal = parseInt(cartTotal) + 1;
+
+    button.addClass("sendtocart");
+    setTimeout(function () {
+      button.removeClass("sendtocart");
+      cart.addClass("shake").attr("data-totalitems", newCartTotal);
       setTimeout(function () {
-        button.removeClass("sendtocart");
-        cart.addClass("shake").attr("data-totalitems", newCartTotal);
-        setTimeout(function () {
-          cart.removeClass("shake");
-        }, 500);
-      }, 1000);
-    });
+        cart.removeClass("shake");
+      }, 500);
+    }, 1000);
   });
-  
+});
